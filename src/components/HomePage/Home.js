2dd9e139081f24f8d4759160ab1e56bc95a1c7ad
Sumonahmed69 <sumonahmed012340@gmail.com>
2021-06-23 13:57:20 +0600

    components upload

---
+++ src/components/HomePage/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '../Footer/Footer';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Navbar</h1>
+            <h1>Header</h1>
+            <h1>Main</h1>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
